87da112b15267821128229f87cdad8b24ca3c097
Ishan Saini <isaini331@gmail.com>
2021-05-28 01:48:31 +0530

    Project Complete---Done

--- src/main/java/com/wolfspring/starterpack/services/JokeServiceImpl.java
+++ src/main/java/com/wolfspring/starterpack/services/JokeServiceImpl.java
@@ -1,13 +1,19 @@
 package com.wolfspring.starterpack.services;
 
 import org.springframework.stereotype.Service;
+import guru.springframework.norris.chuck.*;
 
 @Service
 public class JokeServiceImpl implements JokeService {
+	private final ChuckNorrisQuotes randomJokes;
+	
+	public JokeServiceImpl() {
+		this.randomJokes = new ChuckNorrisQuotes();
+	}
 
 	@Override
 	public String getJoke() {
-		return "Hello World";
+		return randomJokes.getRandomQuote();
 	}
 
 }
